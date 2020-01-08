5efd2ec89db2dc23c2f521de80da634eff83a409
Vincent Letourneau <vincent@nanoninja.com>
2020-01-08 11:48:56 +0100

    Add controllers

--- aston-api/src/controller/UserController.ts
+++ aston-api/src/controller/UserController.ts
@@ -1,13 +1,54 @@
+import * as bcryptjs from 'bcryptjs';
+import { validate, Validator, ValidationError } from 'class-validator';
 import { getRepository } from "typeorm";
 import { NextFunction, Request, Response } from "express";
 import { User } from "../entity/User";
+import { Role } from '../entity/Role';
 
 export class UserController {
 
     private userRepository = getRepository(User);
 
     async register(request: Request, response: Response) {
-        const user = request.body as User;
+        const body = request.body as User;
+        const user = new User();
+
+        user.email = body.email;
+        user.firstName = body.firstName;
+        user.lastName = body.lastName;
+        user.password = body.password;
+        user.birthdate = new Date();
+
+        const errors: ValidationError[] = await validate(user);
+        if (errors.length > 0) {
+            response.status(400);
+            return { error: 'Bad Request', code: 400 };
+        }
+
+        user.password = await bcryptjs.hashSync(user.password);
+
+        const repoRole = getRepository(Role);
+        const role = await repoRole.findOne({ where: { name: 'User' } })
+        if (!role) {
+            response.status(500);
+            return { error: 'Internal Server Error', code: 500 };
+        }
+
+        user.roles = [role];
+
+        try {
+            response.status(201);
+            await this.userRepository.save(user);
+        } catch (error) {
+            if (error.code === 'ER_DUP_ENTRY') {
+                response.status(409);
+                return { error: 'Conflict', code: error.code };
+            }
+            console.log(error);
+            response.status(422);
+            return { error: 'UnprocessableEntity', code: 422 };
+        }
+
         return user;
     }
 
